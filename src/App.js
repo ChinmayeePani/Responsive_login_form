868dddc9f777fed243becf203863661ffe6e39de
ChinmayeePani <cpani1997@gmail.com>
2023-06-30 10:19:22 +0530

    Responsive login form, completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [update, setUpdate] = useState();
+  const [activities, setActivities] = useState([]);
+  const [header, setHeader] = useState("");
+  const [bgcolor, setBgColor] = useState("");
+  const [color, setColor] = useState("");
+  const [listItems, setListItems] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(header, bgcolor, color, listItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="form">
+        <form className="form_content" onSubmit={submitHandler}>
+          <label>HEADER</label>
+          <input
+            type="text"
+            className="text"
+            value={header}
+            onChange={(e) => setHeader(e.target.value)}
+          />
+
+          <label>BACKGROUND-COLOR</label>
+          <input
+            type="text"
+            className="text"
+            value={bgcolor}
+            onChange={(e) => setBgColor(e.target.value)}
+          />
+
+          <label>COLOR</label>
+          <input
+            type="text"
+            className="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+
+          <label>ITEM 1</label>
+          <input
+            type="text"
+            className="text"
+            value={listItems}
+            onChange={(e) => setListItems(e.target.value)}
+          />
+
+          <label>ITEM 2</label>
+          <input
+            type="text"
+            className="text"
+            value={listItems}
+            onChange={(e) => setListItems(e.target.value)}
+          />
+
+          <label>ITEM 3</label>
+          <input
+            type="text"
+            className="text"
+            value={listItems}
+            onChange={(e) => setListItems(e.target.value)}
+          />
+
+          <button className="btn" type="submit">
+            SUBMIT
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
